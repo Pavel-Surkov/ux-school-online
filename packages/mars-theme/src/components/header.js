55c1bb984d8fab2a634ab19ea30fcc437839af8f
Pavel Surkov <surkovv.pavel@gmail.com>
2022-06-30 02:40:01 +0700

    adds flex container and logo changing

--- packages/mars-theme/src/components/header.js
+++ packages/mars-theme/src/components/header.js
@@ -3,6 +3,10 @@ import { connect, styled } from "frontity";
 import Link from "./constant/Link";
 import MobileMenu from "./Menu";
 import Container from "./constant/Container";
+import Image from "@frontity/components/image";
+import logo from "../assets/images/svg/Logo.svg";
+import whiteLogo from "../assets/images/svg/Logo-white.svg";
+import { flex } from "./base/functions";
 
 const Header = ({ state, actions }) => {
   const theme = state.theme.headerTheme;
@@ -13,14 +17,13 @@ const Header = ({ state, actions }) => {
 
   return (
     <>
-      <div>{theme}</div>
-      <Container>
+      <HeaderContainer>
         <StyledLink link="/">
-          <Title>{state.frontity.title}</Title>
+          <Image src={theme === "white" ? whiteLogo : logo} alt="logo" />
         </StyledLink>
         <Description>{state.frontity.description}</Description>
         <MobileMenu />
-      </Container>
+      </HeaderContainer>
     </>
   );
 };
@@ -28,9 +31,8 @@ const Header = ({ state, actions }) => {
 // Connect the Header component to get access to the `state` in it's `props`
 export default connect(Header);
 
-const Title = styled.h2`
-  margin: 0;
-  margin-bottom: 16px;
+const HeaderContainer = styled(Container)`
+  ${flex("row", "center")}
 `;
 
 const Description = styled.h4`

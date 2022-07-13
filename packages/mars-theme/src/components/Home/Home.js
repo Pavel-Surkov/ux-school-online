9cfb5c50ff3c219dda97fe76ae95f60d07d098f4
Pavel Surkov <surkovv.pavel@gmail.com>
2022-07-13 12:52:22 +0700

    adds video

--- packages/mars-theme/src/components/Home/Home.js
+++ packages/mars-theme/src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import Skills from "./Skills/Skills";
 import { connect, styled } from "frontity";
 
 const Home = () => {
   return (
     <HomeElement>
       <Hero />
+      <Skills />
     </HomeElement>
   );
 };
---
+++ packages/mars-theme/src/components/Home/Skills/Skills.js
@@ -0,0 +1,110 @@
+import React from "react";
+import Container from "../../constant/Container";
+import { connect, styled, css } from "frontity";
+
+import poster from "../../../assets/images/about-video.png";
+
+const Skills = () => {
+  return (
+    <SkillsSection>
+      <SkillsContainer>
+        <VideoWrapper>
+          <div
+            css={css`
+              position: relative;
+            `}
+          >
+            <video width="100%" poster={poster}>
+              <source src="" type="video/mp4" />
+            </video>
+            <Play aria-label="play video">
+              <svg
+                width="72"
+                height="72"
+                viewBox="0 0 72 72"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  width="72"
+                  height="72"
+                  rx="36"
+                  fill="white"
+                  fill-opacity="0.1"
+                />
+                <path
+                  d="M46.3487 35.5951C46.6252 35.7946 46.6252 36.2064 46.3487 36.406L31.3395 47.2376C31.0088 47.4763 30.5469 47.24 30.5469 46.8322V25.1689C30.5469 24.7611 31.0088 24.5248 31.3395 24.7634L46.3487 35.5951Z"
+                  fill="white"
+                />
+                <rect
+                  x="0.5"
+                  y="0.5"
+                  width="71"
+                  height="71"
+                  rx="35.5"
+                  stroke="white"
+                  stroke-opacity="0.3"
+                />
+              </svg>
+            </Play>
+          </div>
+        </VideoWrapper>
+        <SkillsBlock></SkillsBlock>
+      </SkillsContainer>
+    </SkillsSection>
+  );
+};
+
+const Play = styled.button`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: transparent;
+  border: none;
+  transition: transform 0.2s;
+  &:hover {
+    transform: translate(-50%, -50%) scale(1.08);
+  }
+  &:active {
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const VideoWrapper = styled.div`
+  position: absolute;
+  top: -272px;
+  left: 0;
+  & video {
+    position: relative;
+    border-radius: 48px;
+  }
+  @media screen and (max-width: 991px) {
+    top: -88px;
+    & video {
+      border-radius: 32px;
+    }
+  }
+  @media screen and (max-width: 576px) {
+    width: calc(100vw - 48px);
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
+const SkillsBlock = styled.div`
+  display: grid;
+  grid-template-columns: calc((408 / 1200) * 100%) calc((792 / 1200) * 100%);
+  padding-top: 552px;
+`;
+
+const SkillsContainer = styled(Container)`
+  position: relative;
+`;
+
+const SkillsSection = styled.section`
+  position: relative;
+`;
+
+export default connect(Skills);

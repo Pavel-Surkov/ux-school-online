410e47bf67ca0d89ea4454798c453fa335f5f4dd
Pavel Surkov <surkovv.pavel@gmail.com>
2022-07-25 19:56:28 +0700

    adds info-slider

--- packages/mars-theme/src/components/Home/Skills/SkillsSwiper/SkillsSwiper.js
+++ packages/mars-theme/src/components/Home/Skills/SkillsSwiper/SkillsSwiper.js
@@ -71,15 +71,49 @@ const SkillsSwiper = ({ state }) => {
 
   return (
     <Content>
+      <PrevBtn className="info-prev">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M15.983 4.14201C15.5436 3.70266 14.8313 3.70266 14.392 4.14201L8.01701 10.517C7.57766 10.9563 7.57766 11.6686 8.01701 12.108L14.392 18.483C14.8313 18.9223 15.5436 18.9223 15.983 18.483C16.4223 18.0436 16.4223 17.3313 15.983 16.892L10.4035 11.3125L15.983 5.73291C16.4223 5.29356 16.4223 4.58136 15.983 4.14201Z"
+            fill="#80859A"
+          />
+        </svg>
+      </PrevBtn>
+      <NextBtn className="info-next">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8.017 18.4829C8.45634 18.9223 9.16866 18.9223 9.60799 18.4829L15.9829 12.108C16.4223 11.6687 16.4223 10.9563 15.9829 10.517L9.60799 4.142C9.16866 3.70267 8.45634 3.70267 8.017 4.142C7.57767 4.58134 7.57767 5.29366 8.017 5.73299L13.5965 11.3125L8.017 16.892C7.57767 17.3314 7.57767 18.0436 8.017 18.4829Z"
+            fill="#80859A"
+          />
+        </svg>
+      </NextBtn>
+      <PaginationElement className="pagination-element" />
       <Swiper
         className="info-slider"
         loop={true}
         modules={[Pagination, EffectFade, Navigation, Controller, Autoplay]}
-        pagination={{ clickable: true }}
+        pagination={{ el: ".pagination-element", clickable: true }}
         effect="fade"
+        navigation={{ prevEl: ".info-prev", nextEl: ".info-next" }}
         controller={{ control: infoImgSwiper }}
         onSwiper={(swiper) => setInfoSwiper(swiper)}
-        speed={1000}
+        speed={800}
         autoplay={{
           delay: 3000,
         }}
@@ -104,7 +138,6 @@ const SkillsSwiper = ({ state }) => {
           );
         })}
       </Swiper>
-
       <ImageSwiperWrapper>
         <Swiper
           className="info-img-slider"
@@ -131,6 +164,46 @@ const SkillsSwiper = ({ state }) => {
   );
 };
 
+const PrevBtn = styled.button`
+  background: transparent;
+  border: none;
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: 168px;
+  left: 16px;
+  z-index: 2;
+  place-items: center;
+  padding: 0;
+  display: none;
+  @media screen and (max-width: 991px) {
+    display: grid;
+  }
+`;
+
+const NextBtn = styled.button`
+  background: transparent;
+  border: none;
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: 168px;
+  right: 16px;
+  z-index: 2;
+  place-items: center;
+  padding: 0;
+  display: none;
+  @media screen and (max-width: 991px) {
+    display: grid;
+  }
+`;
+
+const PaginationElement = styled.div`
+  position: absolute;
+  left: 48px;
+  bottom: 40px;
+`;
+
 const ImageSwiperWrapper = styled.div`
   position: absolute;
   top: -70px;
@@ -155,6 +228,9 @@ const ImageSwiperWrapper = styled.div`
 const Text = styled.div`
   max-width: 360px;
   margin: 0 auto;
+  @media screen and (max-width: 991px) {
+    text-align: center;
+  }
 `;
 
 const SlideTitle = styled(TitleS)`
@@ -166,13 +242,24 @@ const SlideTitle = styled(TitleS)`
   }
 `;
 
-const Note = styled.div``;
+const Note = styled.div`
+  padding-bottom: 20px;
+  @media screen and (max-width: 991px) {
+    padding: 0;
+  }
+`;
 
 const ImageWrapper = styled.div`
   @media screen and (max-width: 1400px) {
     & img {
       max-width: 280px;
       max-height: 280px;
+    }
+  }
+  @media screen and (max-width: 991px) {
+    & img {
+      max-width: 200px;
+      max-height: 200px;
     }
   }
 `;
@@ -200,8 +287,9 @@ const SlideContent = styled.div`
   box-sizing: border-box;
   @media screen and (max-width: 1440px) {
     grid-template-columns: 50% 50%;
+    grid-gap: 0;
   }
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 991px) {
     padding: 24px;
     padding-bottom: 29px;
     grid-template-columns: 100%;
@@ -211,14 +299,29 @@ const SlideContent = styled.div`
 
 const Content = styled.div`
   position: relative;
+  z-index: 1;
+  background: var(--white);
   border-radius: 48px;
   box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.03), 0px 1px 1px rgba(0, 0, 0, 0.1),
     0px 48px 64px rgba(0, 0, 0, 0.05);
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: -12px;
+    transform: translateY(-50%);
+    width: calc(100% + 24px);
+    height: calc(100% - 84px);
+    border-radius: 16px;
+    box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.03), 0px 1px 1px rgba(0, 0, 0, 0.1),
+      0px 48px 64px rgba(0, 0, 0, 0.05);
+  }
   & .info-slider {
+    background: var(--white);
     position: relative;
     border-radius: 48px;
   }
-  & div.swiper-pagination {
+  & div.pagination-element {
     left: 48px;
     bottom: 40px;
     ${flex()};
@@ -244,12 +347,21 @@ const Content = styled.div`
     }
   }
   @media screen and (max-width: 991px) {
+    max-width: 360px;
+    width: calc(100vw - 48px);
+    margin: 0 auto;
+    box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.03), 0px 1px 1px rgba(0, 0, 0, 0.1),
+      0px 48px 64px rgba(0, 0, 0, 0.05);
     & .info-slider {
       border-radius: 32px;
     }
-    & div.swiper-pagination {
-      top: calc(100%);
+    & div.pagination-element {
+      top: calc(100% + 52px);
       bottom: auto;
+      justify-content: center;
+      left: 50%;
+      transform: translateX(-50%);
+      max-width: 100%;
     }
   }
 `;

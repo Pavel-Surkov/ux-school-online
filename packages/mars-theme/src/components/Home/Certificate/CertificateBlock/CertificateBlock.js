659c33e96a1ef4f1ac51a90b9ad9d12e80d87bf8
Pavel Surkov <surkovv.pavel@gmail.com>
2022-08-07 22:39:35 +0700

    adds file for emojies

--- packages/mars-theme/src/components/Home/Certificate/CertificateBlock/CertificateBlock.js
+++ packages/mars-theme/src/components/Home/Certificate/CertificateBlock/CertificateBlock.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import P from "../../../constant/Paragraph";
 import PrimaryBtn from "../../../constant/PrimaryButton";
+import Emoji from "./Emoji/Emoji";
 import { styled, connect } from "frontity";
 import {
   flex,
@@ -14,14 +15,10 @@ import { TitleM } from "../../../constant/Title";
 import certificate from "../../../../assets/images/certificate.svg";
 import certificateMobile from "../../../../assets/images/certificate-mobile.svg";
 
-import emoji1 from "../../../../assets/images/emoji/Emoji1.png";
-import emoji2 from "../../../../assets/images/emoji/Emoji2.png";
-import emoji3 from "../../../../assets/images/emoji/Emoji3.png";
-import emoji4 from "../../../../assets/images/emoji/Emoji4.png";
-import emoji5 from "../../../../assets/images/emoji/Emoji5.png";
-import emoji6 from "../../../../assets/images/emoji/Emoji6.png";
-
 const CertificateBlock = ({ state }) => {
+  const [checkModalOpened, setCheckModalOpened] = useState(false);
+  const [confirmModalOpened, setConfirmModalOpened] = useState(false);
+
   return (
     <Wrapper>
       <CertificateTitle>Сертификат</CertificateTitle>
@@ -43,52 +40,11 @@ const CertificateBlock = ({ state }) => {
           </CertificateBorderInner>
           <CertificateCheck>
             <P>
-              Мы внедрили <CheckBtn>проверку сертификатов</CheckBtn>. Вы можете
-              посмотреть кому и когда выдавали
+              Мы внедрили <CheckBtn on>проверку сертификатов</CheckBtn>.
+              Вы можете посмотреть кому и когда выдавали
             </P>
           </CertificateCheck>
-          {/* <Emoji
-            transform="translate(calc(-50% - 722px)) rotate(-23deg)"
-            top={195}
-            zIndex={0}
-          >
-            <img src={emoji1} alt="emoji" />
-          </Emoji>
-          <Emoji
-            transform="translate(calc(-50% - 483px)) rotate(10deg)"
-            top={-16}
-            zIndex={0}
-          >
-            <img src={emoji3} alt="emoji" />
-          </Emoji>
-          <Emoji
-            transform="translate(calc(-50% - 352px)) rotate(-20deg)"
-            top={371}
-            zIndex={1}
-          >
-            <img src={emoji4} alt="emoji" />
-          </Emoji>
-          <Emoji
-            transform="translate(calc(-50% + 348px)) rotate(11deg)"
-            top={413}
-            zIndex={1}
-          >
-            <img src={emoji5} alt="emoji" />
-          </Emoji>
-          <Emoji
-            transform="translate(calc(-50% + 474px)) rotate(-26deg)"
-            top={18}
-            zIndex={0}
-          >
-            <img src={emoji6} alt="emoji" />
-          </Emoji>
-          <Emoji
-            transform="translate(calc(-50% + 727px)) rotate(30deg)"
-            top={225}
-            zIndex={0}
-          >
-            <img src={emoji2} alt="emoji" />
-          </Emoji> */}
+          {/* <Emoji /> */}
         </CertificateBorder>
       </CertificateWrapper>
       <CourseBtnWrapper>
@@ -97,20 +53,6 @@ const CertificateBlock = ({ state }) => {
     </Wrapper>
   );
 };
-
-const Emoji = styled.div`
-  position: absolute;
-  width: 256px;
-  height: 256px;
-  top: ${({ top }) => (top ? `${top}px` : `0`)};
-  left: 50%;
-  transform: ${({ transform }) => (transform ? transform : "translateX(-50%)")};
-  z-index: ${({ zIndex }) => `${zIndex}`};
-  & img {
-    max-width: 100%;
-    max-height: 100%;
-  }
-`;
 
 const CheckBtn = styled.button`
   padding: 0;

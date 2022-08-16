1ddbe9feff1e00c1ce0de8ccd7ca94abbe45c6b1
Pavel Surkov <surkovv.pavel@gmail.com>
2022-08-16 19:48:52 +0700

    adds mission block

--- packages/mars-theme/src/components/About/Lecturers/Lecturers.js
+++ packages/mars-theme/src/components/About/Lecturers/Lecturers.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "../../constant/Container";
 import P from "../../constant/Paragraph";
 import { TitleS } from "../../constant/Title";
-import { font, stretch, grayRgba } from "../../base/functions";
+import { font, stretch, grayRgba, flex, whiteRgba } from "../../base/functions";
 import { styled, connect } from "frontity";
 
 import teacher from "../../../assets/images/teacher.png";
@@ -111,11 +111,76 @@ const Lecturers = ({ state }) => {
               );
             })}
           </LecturersContent>
+          <InfoBlock>
+            <Mission>Миссия</Mission>
+            <Info>
+              <InfoText>
+                <P size="l">
+                  Сейчас знания быстро устаревают, и учиться имеет смысл только
+                  у тех, кто в этом «варится». Мы каждый день на передовой и
+                  знаем, что сейчас в тренде, какие технологии лучше
+                  использовать, что стоит делать, а что — нет. Наша команда UX
+                  Mind имеет большой опыт работы с клиентами из Европы
+                  и Америки, и этим опытом мы поделимся с тобой.
+                </P>
+                <P size="l">
+                  Так в чём же миссия нашей школы? — Мы хотим научить тебя не
+                  только дизайнить, а ещё и зарабатывать на своих навыках и
+                  актуальных знаниях.
+                </P>
+              </InfoText>
+            </Info>
+          </InfoBlock>
         </Content>
       </Container>
     </Section>
   );
 };
+
+const InfoText = styled.div`
+  & p {
+    margin-bottom: 14px;
+    color: var(--black-900);
+    font-stretch: 109%;
+    font-variation-settings: "GRAD" 0, "slnt" 0, "XTRA" 468, "XOPQ" 96,
+      "YOPQ" 79, "YTLC" 514, "YTUC" 712, "YTAS" 750, "YTDE" -203, "YTFI" 738;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  @media screen and (max-width: 991px) {
+    & p {
+      ${font(21, 32)};
+    }
+  }
+`;
+
+const Info = styled.div``;
+
+const Mission = styled(P)`
+  margin: 0;
+  ${font(14, 20)}
+  ${stretch(110)}
+	color: var(--gray-500);
+  @media screen and (max-width: 991px) {
+    position: absolute;
+    top: 25px;
+    left: 0;
+  }
+`;
+
+const InfoBlock = styled.div`
+  border-top: 1px dashed ${grayRgba(0.2)};
+  padding-top: 137px;
+  display: grid;
+  grid-template-columns: 4fr 6fr 2fr;
+  grid-gap: 24px;
+  position: relative;
+  @media screen and (max-width: 991px) {
+    padding-top: 152px;
+    grid-template-columns: 100%;
+  }
+`;
 
 const Position = styled(P)`
   ${font(14, 20)};
@@ -158,7 +223,6 @@ const LecturersContent = styled.ul`
   grid-template-columns: repeat(6, 1fr);
   grid-row-gap: 48px;
   grid-column-gap: 24px;
-  border-bottom: 1px dashed ${grayRgba(0.2)};
   @media screen and (max-width: 1400px) {
     grid-template-columns: repeat(5, 1fr);
   }

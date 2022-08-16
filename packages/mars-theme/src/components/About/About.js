1feacb4d6240aae920104a7a041118de2705e693
Pavel Surkov <surkovv.pavel@gmail.com>
2022-08-16 18:29:26 +0700

    adds desktop title section

--- packages/mars-theme/src/components/About/About.js
+++ packages/mars-theme/src/components/About/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroVideo from "./HeroVideo/HeroVideo";
+import TitleSection from "./TitleSection/TitleSection";
 import { styled } from "frontity";
 
 const About = () => {
   return (
     <AboutElement>
       <HeroVideo />
+      <TitleSection />
     </AboutElement>
   );
 };

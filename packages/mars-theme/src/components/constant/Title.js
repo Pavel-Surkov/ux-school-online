82aea62f63c502e24c9ab2b037e7a40a765eaf3c
Surkov Pavel <surkovv.pavel@gmail.com>
2022-08-22 11:24:54 +0700

    adds 404 page

--- packages/mars-theme/src/components/constant/Title.js
+++ packages/mars-theme/src/components/constant/Title.js
@@ -22,12 +22,13 @@ export const TitleL = styled.h1`
 
 export const TitleM = styled.h2`
   margin: 0;
-  color: ${({ color }) =>
+  ${({ color }) =>
     color === "white"
-      ? "var(--white)"
+      ? "color: var(--white)"
       : color === "black"
-      ? "var(--black-900)"
-      : gradient()};
+      ? "color: var(--black-900)"
+      : `${gradient()}`};
+
   text-align: ${({ align }) => (align ? align : "left")};
   ${font(52, 56)};
   font-weight: 540;
